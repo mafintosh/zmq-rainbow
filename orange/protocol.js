804e264165e64c01a30a599240710ea4b231f21c
Mathias Buus <mathiasbuus@gmail.com>
2014-03-28 16:21:55 +0100

    fixed protoco

--- orange/protocol.js
+++ orange/protocol.js
@@ -16,38 +16,44 @@ module.exports = function() {
 
 		switch (cmd) {
 			case 1: // sub
-			protocol.emit('subscribe', signature.slice(3).toString());
+			protocol.emit('subscribe', signature.slice(4).toString());
 			return;
 			case 2: // pub
 			if (!content) return protocol.emit('warning', 'invalid message 2');
-			protocol.emit('publish', signature.slice(3).toString(), content);
+			protocol.emit('publish', signature.slice(4).toString(), content);
 			return;
 			case 3: // deliver
 			if (!content) return protocol.emit('warning', 'invalid message 3');
-			protocol.emit('deliver', signature.slice(3).toString(), content);
+			protocol.emit('deliver', signature.slice(4).toString(), content);
 			return;
 			case 4: // wtf
-			protocol.emit('wtf', signature.slice(3).toString());
+			protocol.emit('wtf', signature.slice(4).toString());
 			return;
 		}
 
 		protocol.emit('warning', 'unknown command', cmd);
 	};
 
+	var fromString = function(str) {
+		var buf = new Buffer('0'+str);
+		buf[0] = str.length;
+		return buf;
+	};
+
 	protocol.subscribe = function(channel) {
-		return [Buffer.concat([new Buffer([0xaa, 0x00, 0x01]), new Buffer(channel)])];
+		return [Buffer.concat([new Buffer([0xaa, 0x00, 0x01]), fromString(channel)])];
 	};
 
 	protocol.publish = function(channel, content) {
-		return [Buffer.concat([new Buffer([0xaa, 0x00, 0x02]), new Buffer(channel)]), content];
+		return [Buffer.concat([new Buffer([0xaa, 0x00, 0x02]), fromString(channel)]), content];
 	};
 
 	protocol.deliver = function(channel, content) {
-		return [Buffer.concat([new Buffer([0xaa, 0x00, 0x03]), new Buffer(channel)]), content];
+		return [Buffer.concat([new Buffer([0xaa, 0x00, 0x03]), fromString(channel)]), content];
 	};
 
 	protocol.wtf = function(reason) {
-		return [Buffer.concat([new Buffer([0xaa, 0x00, 0x04]), new Buffer(reason)])];
+		return [Buffer.concat([new Buffer([0xaa, 0x00, 0x04]), fromString(reason)])];
 	};
 
 	return protocol;
