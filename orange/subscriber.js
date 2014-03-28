eb81dc58b31178bbca2972e0bdab2a8c71646c96
Mathias Buus <mathiasbuus@gmail.com>
2014-03-28 13:14:26 +0100

    subscriber prints messages

--- orange/subscriber.js
+++ orange/subscriber.js
@@ -4,5 +4,5 @@ var sock = zmq.socket('dealer');
 sock.connect('tcp://127.0.0.1:3001');
 
 sock.on('message', function(channel, message) {
-	console.log('onmessage', channel.toString());
+	console.log('onmessage', channel.toString(), message.toString());
 });
